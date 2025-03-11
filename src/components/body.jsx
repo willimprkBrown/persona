537220d6b50c2385d5fa6ee2bc7c977773bf0445
William Park <william_c_park@brown.edu>
2025-03-10 21:22:01 -0400

    thirD

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -17,16 +17,16 @@ const questions = [
 ];
 
 const responses = [
-    ["Entered my phone number", "Read the text at the top of the kiosk", "Tapped and started entering my phone number before I realized I could skip this step"],
+    ["Entered my phone number", "Read the text at the top of the kiosk", "Tapped the screen and started entering my phone number before I realized I could skip this step"],
     ["No", "When I clicked on the drink I wanted I tried to add to my bag but it wouldn't let me. I later found out there were required options I had to fill out.", "At first I didn't know what tabs to look at since there were so many options to scroll through and look at."],
-    ["Nothing", "Change the layout to allow for easier sight of all options available", "Change the color and size of text on the screen to make known options more obvious"],
-    ["Nothing", "Show a grouping of the most popular drinks so I know what people like getting here", "If I could read the full description of the menu item without clicking on the item itself"]
+    ["Not sure, the interface is very user-friendly", "Change the layout to make all the options available easier to see and look through", "Change the color and size of text on the screen to make descriptions easier to read"],
+    ["Having a fun button to press instead of just tapping on the screen", "Show a grouping of the most popular drinks so I know what people like getting here", "If I could read the full description of the menu item without clicking on the item itself"]
 ];
 
 const summaries = [
     ["Users tapped on the screen immediately or read the tape at the top and bottom of the kiosk briefly"],
-    ["Users were unsure of which drinks were considered free drinks", "Users had to restart my order to enter my phone number", "Users who did not use points faced no confusion"],
-    ["Making the drinks more discoverable and easier to look through. Avoid having users scroll up and down multiple times.", "Describe the benefits of points all at once instead of slides."],
+    ["Users were not aware of required options to select when customizing drink", "Too many categories and options to look through"],
+    ["Making the drinks more discoverable and easier to look through. Avoid having users scroll up and down multiple times.", "Make the descriptions of the drinks easier to read or access."],
     ["Knowing what drinks are most popular and most ordered to avoid having to choose and look through all of the options", "Having a start button on the main screen makes entering more satisfying"]
 ];
 
@@ -47,20 +47,21 @@ const Body = () => {
         <section className="body">
             <h1>Persona Project</h1>
 
-            <p>This project will look at the experiences of users with the Ten One Tea House ordering kiosk. User intereactions
+            <p>This project will look at the experiences of users with the Ten One Tea House ordering kiosk. User interactions
             with the kiosk will be observed and users will be interviewed on their experiences with the interface. From these
             observations and interviews, personas will be created to represent the different types of users that interact with
             the kiosk. These personas will be used to create a storyboard that will show how certain users are expected to interact with
             the kiosk.</p>
 
             <h1>Interface</h1>
-            <p>The interface I choose to base my project on is the Ten One Tea House ordering kiosk. Below is a simplified diagram with the key compoenents of the interface:</p>
+            <p>The interface I chose to study was the Ten One Tea House ordering kiosk, because it is an interface I interact with often. Below is a simplified diagram with the key compoenents of the interface:</p>
             <img src={interfaceImage} alt="Kiosk Interface" className="kiosk-image" />
             <p className='center'>The kiosk is trying to reduce the workload of drink orders on workers and streamline the ordering process for users. The interface is a physical kiosk that is attached to the kisok stand
-            The kiosk interface is a touch screen that allows users to select and order drinks from the Ten One Tea House menu. The interface has several digital components (ex. buttons) that allow users to navigate through the menu, 
+            The kiosk interface is a touch screen that allows users to select and order drinks from the Ten One Tea House menu. The interface has several digital components (ex. buttons, side menu) that allow users to navigate through the menu, 
             select and customize drinks, and prompt the user to make a payment. There is then a card swiper/scanner to allow users to make payment transactions.</p>
             <h1>User Responses</h1>
-            <p>3 users were interviewed and asked the following questions:</p>
+            <p>To gather more information on how this interface is being interacted with, more specifically what about the interface is good and what could be improved.
+            I gathered data by obsevring and interviewing 3 users after they used the kiosk, asking the following questions:</p>
             <div className="table-container">
                 <table>
                     <thead>
@@ -84,7 +85,7 @@ const Body = () => {
                 </table>
             </div>
             <h1>Response Summaries</h1>
-            <p>A summary of user responses:</p>
+            <p>I then summarized the responses from the users to see overarching trends in what users found good in the interface and what needs improvements.</p>
             <div className="table-container">
                 <table>
                     <thead>
@@ -110,7 +111,9 @@ const Body = () => {
                 </table>
             </div>
             <h1>User Action Observations</h1>
-            <p>An objective list of actions that users took while interacting with the kiosk:</p>
+            <p>During the study I also made an objective list of actions that users took while interacting with the kiosk to
+                further help in my study of the interface, splitting my observations into how users navigated the interface, and 
+                decisions they made within it.</p>
             <div className="table-container">
                 <table>
                     <thead>
@@ -140,9 +143,8 @@ const Body = () => {
                 </table>
             </div>
             <h1>Personas</h1>
-            <p>Based on the observations and responses of users, I will be creating two personas that represent different
-                types of customers that may interact with the Ten One Tea House kiosk. These personas will also have emptahy maps, detailing
-                the personas's thoughts, feelings, and actions when interacting with the kiosk. The two personas are Vivienne and Tom.
+            <p> To try to help understand how users may interact with the kiosk interface from even more perspectives, I created two personas that represent different types of customers that may interact with the Ten One Tea House kiosk based on my observations and responses of users . 
+                I also created emptahy maps for each persona which detail the personas's thoughts, feelings, and actions when interacting with the kiosk. The two personas are Vivienne and Tom.
             </p>
             <h2>Vivienne</h2>
             <img src={viviennePicture} alt="Vivienne" className="profile-image" />
@@ -167,8 +169,16 @@ const Body = () => {
             <img src={tomMap} alt="Tom Empathy Map" className="map-image" />
 
             <h1>Storyboard</h1>
-            <p>This storyboard will follow the Tom persona, detailing how Tom would interact with the kiosk interface from start to finish</p>
+            <p>To understand how these personas would interact with the interface in a visual way, I created a storyboard that follows the Tom persona, 
+                detailing how Tom would interact with the kiosk interface from start to finish</p>
             <img src={storyboardImage} alt="Storyboard" className="storyboard-image" />
+
+            <h1>Conclusions</h1>
+            <p>After observing and interviewing users in-person and creating personas to gain further perspective on the Ten One Tea House kiosk interface,
+                I was able to gain a better understanding of how a variety of users interact with the interface and what could be improved. Before collecting data
+                from users and creating personas, my personal experience with the kiosk was that it was easy to use and navigate. However after conducting this study, I realized that the interface 
+                could be improved to make the ordering process more efficient and enjoyable for many different types users.
+            </p>
 
         </section>
     );
